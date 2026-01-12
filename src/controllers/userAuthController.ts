bc97ada681b50b7e736ef61e0c2a4a5ec48accb6
Pablo Romano <patoromano22@gmail.com>
2026-01-12 01:00:55 -0300

    comentar email verified en login

--- src/controllers/userAuthController.ts
+++ src/controllers/userAuthController.ts
@@ -108,9 +108,9 @@ export async function firebaseLogin(req: FastifyRequest, reply: FastifyReply) {
         const decoded = await req.server.firebase.auth().verifyIdToken(idToken, true)
         const { uid, email_verified } = decoded
 
-        if (email_verified === false) {
-            return reply.code(401).send({ message: 'Email no verificado en Firebase' })
-        }
+        // if (email_verified === false) {
+        //     return reply.code(401).send({ message: 'Email no verificado en Firebase' })
+        // }
 
         // 2. Buscar usuario en la base de datos
         const user = await findUserByFirebaseUid(uid)
